85ca41b4f84bbe840ba1c5f491daccf23445dd78
Jeppe Larsen <veirum@gmail.com>
2022-11-16 09:59:01 +0100

    Update

---
+++ basics/sketch.js
@@ -0,0 +1,19 @@
+//IF-STATEMENTS
+
+//Er udsagnet i () sandt?
+if (3 > 1) {
+  //Hvis sandt eksekveres koden
+  //mellem de to { } eksekveres
+  console.log("Det er sandt at 3 > 1");
+}
+
+//If kan sættes sammen med else
+// for enten eller
+
+if (3 > 4) {
+  //Hvis sandt eksekveres koden
+  //mellem de to { } eksekveres
+} else {
+  //ellers køres koden i else
+  console.log("Det er ikke sandt at 3 > 4");
+}
